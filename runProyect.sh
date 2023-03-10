aad4140d297db4bb8e71e77c52dbffb918a4087d
Jesus_B <17-10046@usb.ve>
2023-03-10 19:38:51 -0400

    removed testing.sh

---
+++ runProyect.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Copyright (c) 2012-2022, EnterpriseDB Corporation.  All rights reserved
+
+# PostgreSQL psql runner script for OS X
+
+echo -n "Username [postgres]: "
+read USERNAME
+
+if [ "$USERNAME" = "" ];
+then
+    USERNAME="postgres"
+fi
+
+echo -n "Database [postgres]: "
+read DATABASE
+
+while [ "$DATABASE" = "" ];
+do
+    echo "Debe indicar el nombre de la DATABASE a crear"
+    echo -n "Database [postgres]: "
+    read DATABASE
+done
+
+echo -n "Server [localhost]: "
+read SERVER
+
+if [ "$SERVER" = "" ];
+then
+    SERVER="localhost"
+fi
+
+echo -n "Port [5432]: "
+read PORT
+
+if [ "$PORT" = "" ];
+then
+    PORT="5432"
+fi
+
+
+createdb  -h $SERVER -p $PORT -U $USERNAME $DATABASE
+psql -h $SERVER -p $PORT -U $USERNAME $DATABASE -a -f "querys.sql"
+RET=$?
+
+if [ "$RET" != "0" ];
+then
+    echo
+    echo -n "Press <return> to continue..."
+    read dummy
+fi
+
+exit $RET
--- testing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
- 
-#Set the value of database
-database="jesus" 
- 
-psql -U "jesus" -d $database -a -f "querys.sql"
